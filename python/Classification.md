1d0dc2938dd7d83f1138102d4720e881904b8900
eusoubrasileiro <eusoubrasileiro@gmail.com>
2024-09-12 16:16:16 -0300

    already a workable classification model - creating features using a window of 10 seconds - can be less - entropy features added some 2% or 3% on mean accuracy

--- python/Classification.md
+++ python/Classification.md
@@ -1,20 +1,25 @@
 ### Indentifiying/Classifing video streaming traffic features for classification.
 
-Get all ips within lan and ...
-First of ALL: Identify the client the packets are talking about!
+First identify clients on my LAN (local network). 
+Using subnet 192.168.0.0/24 - 32 - 24 = 8 bits - 256 clients is simple.
+Ignoring broadcast address (router) 192.168.0.1.
  
 ### Within a 5 seconds window calculate:
 
-The problem is that during 5 seconds we might get packets from different clients. 
-Problem solved above. 
+ 1. Identify all clients that had traffic of packets during this period 
+ 2. For each packet identify to which client it belogs (from source or destination ip) 
 
- - calculate average upload speed for specific client?
- - calculate average upload speed for specific client?
- - packet size variance: get all packets sizes received and calculate the variance
- - packet size average : get all packets sizes and make an average
- - packet average delay time: delay between received packets - get all packets time differences between then and make an average
- - packet variance delay time: the variance of the previous mesurament
- Connection Multiplexing - many video streaming open multiple connections to different servers or CDNS's simultaneously. 
- - for every packet get source ip and destination ip, store all the ips involved and calculate the nunique ips. 
- - Group the top n (10) and get their traffic average volume. ?
+ - With the packet data and for each specific client:
+    - calculate average upload speed for specific client
+    - calculate average download speed for specific client
+    - packet size variance: get all packets sizes received and calculate the variance
+    - packet size average : get all packets sizes and make an average
+    - packet average delay time: delay between received packets - packet arrival time difference make an average
+    - packet variance delay time: the variance of the previous mesurament    
+    - *Connection Multiplexing*: many video streaming open multiple connections to different servers or CDNS's simultaneously. 
+        - for every packet get source ip and destination ip, store all the ips involved and calculate the nunique ips. 
+        - Group the top n (10) and get each one of them the traffic average volume. 
 
+### Training and Classification
+
+For creating training data use a specific local machine, no mix of clients. For classification use the lan router local network interface and many clients can be classified at once at every 5 seconds. 
--- python/scapy_sniffer.py
+++ python/scapy_sniffer.py
@@ -1,27 +1,60 @@
-# using remote router openwrt
+# usage with remote router openwrt
 # ssh root@router 'tcpdump -i br-lan -s 1024 -U -w - port 80 or port 443' | tee capture.pcap | python3 scapy_sniffer.py
 # or using local machine for simplicity
 # sudo tcpdump -i wlp2s0 -s 1024 -w - port 80 or port 443 | python3 scapy_sniffer.py
 
-from scapy.all import PcapReader, IP, TCP
+from scapy.all import PcapReader, IP, TCP, UDP
 import sys
 import datetime 
+import argparse
 
-def process_packet(packet):
+def process_packet(packet, training):
     if packet.haslayer(IP):
         ip_layer = packet[IP]
         original_size = ip_layer.len  # Original packet size from IP header
-        print(f"IP {ip_layer.src} -> {ip_layer.dst} Packet Size: {original_size}" 
-              f" Time: {datetime.datetime.fromtimestamp(float(packet.time))}")        
-        # if packet.haslayer(TCP):
-        #     tcp_layer = packet[TCP]
-        #     print(f"Port {tcp_layer.sport} -> {tcp_layer.dport}")
-        
+        identification = ip_layer.id
+        ttl = ip_layer.ttl
+        ip_flags = ip_layer.flags if ip_layer.flags else "NONE"
+
+        # Initialize TCP/UDP fields
+        tcp_sport, tcp_dport, tcp_seq, tcp_ack, tcp_flags = -1, -1, -1, -1, "NONE"
+        udp_sport, udp_dport = -1, -1
+
+        if packet.haslayer(TCP):
+            tcp_layer = packet[TCP]
+            tcp_sport, tcp_dport = tcp_layer.sport, tcp_layer.dport
+            tcp_seq = tcp_layer.seq
+            tcp_ack = tcp_layer.ack
+            tcp_flags = tcp_layer.flags if tcp_layer.flags else "NONE"
+
+        if packet.haslayer(UDP):
+            udp_layer = packet[UDP]
+            udp_sport, udp_dport = udp_layer.sport, udp_layer.dport
+
+        if training:
+            line = ( f"{ip_layer.src:>15} {ip_layer.dst:>15} {original_size:6d} "
+                     f"{tcp_sport:6d} {tcp_dport:6d} {udp_sport:6d} {udp_dport:6d} "
+                     f"{packet.time:18.6f} {identification:5d} {ttl:3d} "
+                     f"{str(ip_flags):>4} {tcp_seq:10d} {tcp_ack:10d} {str(tcp_flags):>4} ")
+            print(line) # stdout -> for training file
+            print(line, file=sys.stderr) # for debugging
+        else:                
+            print(f"packet ips src {ip_layer.src:>15} dst {ip_layer.dst:>15} size {original_size:6d} " 
+                f"tcp sport {tcp_sport:6d} dport {tcp_dport:6d} udp sport {udp_sport:6d} dport {udp_dport:6d} "
+                f"time {packet.time:18.6f} "
+                f"ID:{identification:5d} TTL:{ttl:3d} "
+                f"IP Flags:{str(ip_flags)} TCP Seq:{tcp_seq:10d} TCP Ack:{tcp_ack:10d} TCP Flags:{str(tcp_flags)}"
+                f"hour {datetime.datetime.fromtimestamp(float(packet.time)).isoformat()}")        
+            
 
 def main():
+    parser = argparse.ArgumentParser(description="network traffic packet metadata sniffer")
+    parser.add_argument("--train", action="store_true", help="Print metadata for creating training samples")
+    args = parser.parse_args()
+
     with PcapReader(sys.stdin.buffer) as pcap_reader:  # Use buffer to handle binary data
         for packet in pcap_reader:
-            process_packet(packet)
+            process_packet(packet, args.train)
 
 if __name__ == "__main__":
     main()
